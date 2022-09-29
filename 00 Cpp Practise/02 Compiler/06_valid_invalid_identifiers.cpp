f8e6bc8ab83e82a9777b37e169f72ef9fe6b53a8
Tansen <aatansen@gmail.com>
2022-09-29 16:56:01 +0600

    Create 06_valid_invalid_identifiers.cpp

---
+++ 00 Cpp Practise/02 Compiler/06_valid_invalid_identifiers.cpp
@@ -0,0 +1,36 @@
+// Check the given string is a valid or invalid identifier:
+
+#include <iostream>
+using namespace std;
+bool isValid(string str, int n)
+{
+
+    // Check if first character is invalid
+    if (!((str[0] >= 'a' && str[0] <= 'z') || (str[0] >= 'A' && str[0] <= 'Z') || (str[0] == '_')))
+        return false;
+
+    // Check other characters are invalid
+    for (int i = 1; i < n; i++)
+    {
+        if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9') || (str[i] == '_')))
+            return false;
+    }
+
+    // String is a valid identifier
+    return true;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter a string: ";
+    getline(cin,str);
+    int n = str.length();
+
+    if (isValid(str, n))
+        cout << "Valid";
+    else
+        cout << "Invalid";
+
+    return 0;
+}
