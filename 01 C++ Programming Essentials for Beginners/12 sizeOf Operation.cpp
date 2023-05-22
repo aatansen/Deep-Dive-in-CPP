bb141aa952ccdb61627805f5982876486c6bfd9b
Md. Alahi Almin Tansen <aatansen@gmail.com>
2023-05-22 20:17:26 +0600

    Create 12 sizeOf Operation.cpp

---
+++ 01 C++ Programming Essentials for Beginners/12 sizeOf Operation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Int: " << sizeof(int) <<" Bytes"<< endl;
+    cout << "Char: " << sizeof(char) <<" Bytes"<< endl;
+    cout << "Bool: " << sizeof(bool) <<" Bytes"<< endl;
+    cout << "Float: " << sizeof(float) <<" Bytes"<< endl;
+    cout << "Double: " << sizeof(double) <<" Bytes"<< endl;
+    return 0;
+}
